d8cba9374134105462a10f4af5f8074d1409db7b
Yasukastsu.A <handbuuuu@gmail.com>
2019-09-04 00:18:37 +0900

    コメントアウト追加

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+// 4.Reactの基本ライブラリをインポート
 import React from 'react';
+// 3.ロゴ画像/スタイルシートをインポート
 import logo from './logo.svg';
 import './App.css';
 
+// 1.Appコンポーネントを定義
 function App() {
+  // 2.描写内容を準備
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Reactへようこそ！</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -22,5 +27,5 @@ function App() {
     </div>
   );
 }
-
+// 5.Appクラスをエクスポート
 export default App;
