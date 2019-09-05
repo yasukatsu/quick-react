83b377c6d6e7455aa91b1eefc968346f6c280475
Yasukastsu.A <handbuuuu@gmail.com>
2019-09-06 00:13:26 +0900

    とりあえずいじいじ

--- src/index.js
+++ src/index.js
@@ -9,10 +9,29 @@ import App from './App';
 // 3.キャッシュのためのサービスをインポート
 import * as serviceWorker from './serviceWorker';
 
+const tag = (
+    <label htmlFor="name" className="sky">
+        <input id="name" type="text" tabIndex="0" />
+    </label>
+)
+const str = 'Hello, World!! <br /> goodbye, me'
+const style = {color: 'Red', backgroundColor: 'Yellow'};
+const attrs = {
+    src: 'http://www.wings.msn.to/image/wings.jpg',
+    alt: '',
+    title: 'WINGSプロジェクト'
+};
+
 // 4.Appコンポーネントを実行
 setInterval(() => {
     ReactDOM.render(
-            <App />,
+        <React.Fragment>
+            <img {...attrs} />
+            <p dangerouslySetInnerHTML={{__html: str}}></p>
+            <p style={style}>プロジェクト</p>
+            <p>{tag}さん！Reactを始めよう！</p>
+            <App />
+        </React.Fragment>,
         document.getElementById('root')
     );
 }, 1000);
