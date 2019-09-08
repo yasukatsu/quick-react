dc49c9502b0efe0b7657e7889410fb4817de2b7b
Yasukastsu.A <handbuuuu@gmail.com>
2019-09-08 22:11:08 +0900

    様々な型の値をコンポーネントに渡す

--- src/index.js
+++ src/index.js
@@ -9,11 +9,19 @@ import MyHello from './MyHello';
 
 // 3.キャッシュのためのサービスをインポート
 import * as serviceWorker from './serviceWorker';
+import MyType from './MyType';
 
 // 4.Appコンポーネントを実行
 setInterval(() => {
     ReactDOM.render(
         <React.Fragment>
+            <MyType value = "山田" />
+            <MyType value = {'鈴木'} />
+            <MyType value = {108} />
+            <MyType value = {true} />
+            <MyType value = { ['うめ', 'もも', 'さくら'] } />
+            <MyType value = { {name: "山田太郎", age: "25"} }/>
+            <MyType value = {() => console.log('Hoge')}/>
             <App />
         </React.Fragment>,
         document.getElementById('root')
