57cd8ecb913588799117813fb9c9bc4142ce3def
Yasukastsu.A <handbuuuu@gmail.com>
2019-09-14 13:22:04 +0900

    functionalization

--- src/MyArticle.js
+++ src/MyArticle.js
@@ -2,13 +2,20 @@ import React, {Component} from 'react';
 import MyNew from './MyNew';
 
 export default class MyArticle extends Component {
+    // 条件分岐によるコード
+    renderIfNew(isNew) {
+
+        if (isNew) {
+            return <MyNew />
+        }
+    }
     render() {
         return(
             // 2. dt, dd要素を括るダミーの要素
             <React.Fragment>
                 <dt>
                     <a className="App-link" href= {this.props.url}>{this.props.title}</a>
-                    {this.props.isNew ? <MyNew /> :null}
+                    {this.renderIfNew(this.props.isNew)}
                 </dt>
                 <dd>
                     {this.props.description}
